e0a8ee77bebd204d3ca889c6d386c399c577ee13
Bruno Gomes <brunongomes8@gmail.com>
2025-05-21 15:25:19 -0300

    feat: configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PlansComponent } from './pages/plans/plans.component';
+import { CardFormComponent } from './pages/card-form/card-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'plans', pathMatch: 'full' },
+    { path: 'plans', component: PlansComponent },
+    { path: 'add-card', component: CardFormComponent }
+];
